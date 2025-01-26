e1529043310104968cd549fdbba8be82dec2e897
Diego <diegovalrus@gmail.com>
2025-01-26 16:18:33 -0500

    Scroll Fixed

--- js/scroll_events.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {            
-    // BLURRY - MOBILE
-    const background = document.querySelector('.port_mobile');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 90) background.classList.add('blur')
-        else background.classList.remove('blur');
-    });
-});
